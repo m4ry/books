c6dfa6ce20d8a1dbe626824e43968d337d7a7d98
Marlo Longley <maryelongley@gmail.com>
2011-07-24 01:14:00 -0700

    trying...

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,9 @@ default_run_options[:pty] = true
 # be sure to change these
 set :user, 'webghostingshell'
 set :domain, 'm.xn--ycee.net'
-set :application, 'testa00'
+set :application, 'testapp'
+set :ssh_options, {:forward_agent => true}
+
 
 # the rest should be good
 set :repository, "git@github.com:m4ry/books.git"
